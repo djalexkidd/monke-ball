f3d28b18c586372626a16f06da4903c1e742e69f
djalexkidd <alexpyr@outlook.fr>
2022-02-16 09:17:02 +0100

    Texture and rotation

--- scripts/Steve.gd
+++ scripts/Steve.gd
@@ -1,7 +1,8 @@
 extends KinematicBody
 
 var velocity = Vector3(0,0,0)
-const SPEED = 20
+const SPEED = 6
+const ROTSPEED = 7
 
 func _ready():
 	pass
@@ -12,7 +13,9 @@ func _physics_process(delta):
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
+		$MeshInstance.rotate_z(deg2rad(-ROTSPEED))
 	elif Input.is_action_pressed("ui_left"):
+		$MeshInstance.rotate_z(deg2rad(ROTSPEED))
 		velocity.x = -SPEED
 	else:
 		velocity.x = lerp(velocity.x,0,0.1)
@@ -21,9 +24,12 @@ func _physics_process(delta):
 		velocity.z = 0
 	elif Input.is_action_pressed("ui_up"):
 		velocity.z = -SPEED
+		$MeshInstance.rotate_x(deg2rad(-ROTSPEED))
 	elif Input.is_action_pressed("ui_down"):
 		velocity.z = SPEED
+		$MeshInstance.rotate_x(deg2rad(ROTSPEED))
 	else:
 		velocity.z = lerp(velocity.z,0,0.1)
+	
 	
 	move_and_slide(velocity)
